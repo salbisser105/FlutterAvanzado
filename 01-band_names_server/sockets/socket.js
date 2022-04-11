a5e20c4a0ca360ab412569b7b4ba6be72f06ea49
Santiago_Albisser <salbisserc@eafit.edu.co>
2022-04-10 19:16:16 -0500

    manejo servidor sockets

--- 01-band_names_server/sockets/socket.js
+++ 01-band_names_server/sockets/socket.js
@@ -1,11 +1,19 @@
 const {
     io
 } = require('../index');
-
+const Bands = require('../models/bands_collection');
+const Band = require('../models/band')
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('SpongeBob'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('AC/DC'));
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -19,6 +27,24 @@ io.on('connection', client => {
         });
 
     });
+    client.on('emitir-mensaje', (payload) => {
+        console.log(payload);
+        io.emit('nuevo-mensaje', payload); // Emite a todos 
+        //client.broadcast.emit('nuevo-mensaje', payload); emite a todos menos el que lo metio
+    })
 
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
 
+    });
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+    client.on('delete-band', (payload) => {
+        bands.deleteBands(payload.id);
+        io.emit('delete-bands', bands.getBands());
+    });
 });
